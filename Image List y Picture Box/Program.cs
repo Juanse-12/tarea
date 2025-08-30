aa2aab4578411ccddf9a6a1438954bffcaa1db46
Juanse-12 <jv6192400@gmail.com>
2025-08-30 09:26:29 -0600

    Add files via upload

---
+++ Image List y Picture Box/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Windows.Forms;
+using ImageListYPictureBox;
+
+namespace ImageListYPictureBox
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form2());
+        }
+    }
+}
